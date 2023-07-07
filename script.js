ade0e84053cfca7b5a574e5b4e29f2220bf83dbd
R0HIT-FS <125055656+R0HIT-FS@users.noreply.github.com>
2023-07-07 17:50:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+function show(){
+    switch (key) {
+        case value:
+            
+            break;
+    
+
+                 break;
+    }
+}
+
+
+
+
+
+
+
+
+// else{
+//     let i=0;
+//     while (i<length.value) {
+//         pass += chars[Math.floor(Math.random()*chars.length)];
+//         pass += numbers[Math.floor(Math.random()*numbers.length)];
+//         pass += symbols[Math.floor(Math.random()*symbols.length)];
+//         i += 1;
+//     }
+//     pass=pass.substring(0,length.value)
+//     display.value = pass
+// }
